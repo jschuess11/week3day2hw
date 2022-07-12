c149e2e9c009035ba03b67e9c8b1cbfbd4ab5f90
jschuess11 <108285498+jschuess11@users.noreply.github.com>
2022-07-12 19:01:57 -0400

    hw

---
+++ app.js
@@ -0,0 +1,11 @@
+function number1Function (a, b) {return a + b};
+console.log(a, b);
+
+function number2Function (a, b) {return a - b};
+console.log(a, b);
+
+function number3Function (a, b) {return a * b};
+console.log(a, b);
+
+function number4Function (a, b) {return a / b};
+console.log(a, b);
